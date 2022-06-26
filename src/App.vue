07eed6d60ae7508dacc6cbbeb517e3f56e5db39c
Sergey Ganous <sergeyg@hades.local>
2022-06-26 15:15:32 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import InputForm from './components/InputForm.vue'
+</script>
+
+<template>
+  Hello there!
+  <InputForm />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/InputForm.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <input class="u-full-width" type="text" id="name" placeholder="Name">
+  <textarea></textarea>
+</template>
