485e657486c62179a219183de4148e5e813751ef
Sergey Ganous <sergeyg@hades.local>
2022-06-26 17:23:31 -0700

    Add an initial post interface

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,10 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
 import InputForm from './components/InputForm.vue'
 </script>
 
 <template>
-  Hello there!
   <InputForm />
 </template>
 
--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -1,14 +1,34 @@
 <script>
-export default {
-  data() {
-    return {
-      count: 0
+  export default {
+    data() {
+      return {
+        count: 0
+      }
     }
   }
-}
 </script>
 
+<style scoped>
+  .container {
+    width: 600px;
+  }
+  .button {
+    float: right;
+  }
+</style>
+
 <template>
-  <input class="u-full-width" type="text" id="name" placeholder="Name">
-  <textarea></textarea>
+  <div class="container">
+    <div class="field">
+      <label class="label">Write a post!</label>
+      <div class="control">
+        <textarea class="textarea has-fixed-size" placeholder="Textarea"></textarea>
+      </div>
+    </div>
+    <div class="field">
+      <div class="control">
+        <button class="button is-link">Submit</button>
+      </div>
+    </div>
+  </div>
 </template>
